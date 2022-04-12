1af735a9b284efdbd047a4e8f2d003e4987370f8
blackpintz <rwwanjohi10@gmail.com>
2022-04-12 23:23:57 +0300

    add meta tags for twitter card.

--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -1,12 +1,24 @@
 import { Link } from "react-router-dom";
 import {Helmet} from 'react-helmet';
+import coinImg from '../assets/coin.jpg'
 
 export default function InfoPage() {
+    const hostName = "https://meta-checker.herokuapp.com/"
     return(
         <>
             <Helmet>
                 <title>Info-Page</title>
-                <meta name="description" content="This is the information page." />
+                <meta property="og:title" content="Info-Page" />
+                <meta name="description" content="This is the info page." />
+                <meta property="og:description" content="This is the info page." />
+                <meta property="og:image" content={hostName+coinImg} />
+                <meta
+                property="og:url"
+                content={hostName + window.location.pathname + window.location.search}
+                />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:image:alt" content="coin img" />
+                <meta name="twitter:site" content="@BlockchainHut" />
             </Helmet>
             <h2>This is the information page.</h2>
             <Link to="/another-page">Go to another page.</Link>
