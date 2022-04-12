abf65271681a4f8b7c53af1c4acf37763245a90d
blackpintz <rwwanjohi10@gmail.com>
2022-04-12 12:42:16 +0300

    setup react-helmet.

--- src/components/AnotherPage.js
+++ src/components/AnotherPage.js
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+import {Helmet} from 'react-helmet';
 
-import { Link } from "react-router-dom"
 export default function AnotherPage() {
     return(
         <>
+            <Helmet>
+                <title>Another page</title>
+                <meta name="description" content="This is another page." />
+            </Helmet>
             <h2>This is another page.</h2>
             <Link to="/">Go to Home page.</Link>
         </>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import {Helmet} from 'react-helmet';
+
 export default function HomePage() {
     return(
         <>
+            <Helmet>
+                <title>Home</title>
+                <meta name="description" content="This is the Home page." />
+            </Helmet>
             <h2>This is the home page.</h2>
             <Link to="/info-page">Go to information page.</Link>
         </>
