2666393a8f616d9dbc17d7db67a2ff96f3548b8c
blackpintz <rwwanjohi10@gmail.com>
2022-04-12 16:12:10 +0300

    add meta tag in information page.

--- src/components/InfoPage.js
+++ src/components/InfoPage.js
@@ -1,7 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import {Helmet} from 'react-helmet';
+
 export default function InfoPage() {
     return(
         <>
+            <Helmet>
+                <title>Info-Page</title>
+                <meta name="description" content="This is the information page." />
+            </Helmet>
             <h2>This is the information page.</h2>
             <Link to="/another-page">Go to another page.</Link>
         </>
